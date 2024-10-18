61c446c3744a3ec5d917329b7473a5b59cbdff78
Eunseo Kim <dmstj9408@gamil.com>
2024-10-18 18:33:13 +0900

    [finish]finish app

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+require('dotenv').config()
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors());
 app.use('/api', indexRouter)
 
 // mongoose setting
-const mongoURI = `mongodb://localhost:27017/todolist-demo`
-// mongoose.connect(mongoURI)//,{useNewUrlParser:true}
+const mongoURI = process.env.MONGODB_URI_PROD;
+// console.log('mongoURI', mongoURI)
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 .then(() => {
